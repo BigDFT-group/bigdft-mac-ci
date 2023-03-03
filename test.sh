764a546cebcce88dd4a08437aabb9b6ff958295d
William Dawson <william.dawson@riken.jp>
2023-03-03 16:10:24 +0900

    Add tests

---
+++ test.sh
@@ -0,0 +1,12 @@
+set -ex
+
+# Setup the environment
+source bigdftvars.sh
+
+# Check a few of the useful tools
+bigdft -h
+bigdft-tool -h
+bader -h
+
+# Check that PyBigDFT is Working
+python test.py
