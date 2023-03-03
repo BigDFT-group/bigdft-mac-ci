a17720d97a1dbcabb742c67bb9b7fc7af120e787
William Dawson <william.dawson@riken.jp>
2023-03-03 14:26:49 +0900

    Little more

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 echo "Hello World!"
 git clone https://gitlab.com/l_sim/bigdft-suite.git
 mkdir build
+cd build
 python ../bigdft-suite/Installer.py -y autogen 
+
+brew install lapack
+brew install open-mpi
+which mpif90
+
