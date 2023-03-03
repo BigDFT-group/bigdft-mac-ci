34a66497baf40b5610c1644e73e8187272f9936a
William Dawson <william.dawson@riken.jp>
2023-03-03 14:39:16 +0900

    Try automake

--- install.sh
+++ install.sh
@@ -1,13 +1,9 @@
 echo "Hello World!"
 
-which autoreconf
-autoreconf --version
 # Brew dependencies
 brew install lapack
 brew install open-mpi
-brew install autoreconf
-which autoreconf
-autoreconf --version
+brew install automake
 
 # Get from git
 git clone https://gitlab.com/l_sim/bigdft-suite.git
