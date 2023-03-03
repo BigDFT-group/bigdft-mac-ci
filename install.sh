64e7541e5f715a771892326b300246487ded45fe
William Dawson <william.dawson@riken.jp>
2023-03-03 19:05:08 +0900

    matrix over compilers

--- install.sh
+++ install.sh
@@ -1,43 +1,22 @@
-
-##############################################################################
 # Setup your computer
-##############################################################################
 brew reinstall gcc
 brew link --overwrite gcc
 brew install lapack
 brew install open-mpi
 brew install automake
 
-##############################################################################
 # Get BigDFT from Git
-##############################################################################
 git clone https://gitlab.com/l_sim/bigdft-suite.git
 
-##############################################################################
-# Option 1: Build with GCC
-##############################################################################
+# Build
 
 # Build directory
-mkdir build_gcc
-cd build_gcc
+mkdir build_${1}
+cd build_${1}
 
 # Autogen + build
 python3 ../bigdft-suite/Installer.py -y autogen 
 python3 ../bigdft-suite/Installer.py -y build \
-         -f ../macos_gcc.rc 
-cd ../
-
-##############################################################################
-# Option 2: Build with Clang
-##############################################################################
-
-# Build directory
-mkdir build_clang
-cd build_clang
-
-# Autogen + build
-python3 ../bigdft-suite/Installer.py -y autogen 
-python3 ../bigdft-suite/Installer.py -y build \
-         -f ../macos_clang.rc 
+         -f ../macos_${1}.rc 
 cd ../
 
--- test.sh
+++ test.sh
@@ -1,19 +1,6 @@
 set -ex
 
-##############################################################################
-# Option 1: GCC
-##############################################################################
-source build_gcc/install/bin/bigdftvars.sh
-
-bigdft -h
-bigdft-tool -h
-bader -h
-python test.py
-
-##############################################################################
-# Option 2: Clang
-##############################################################################
-source build_clang/install/bin/bigdftvars.sh
+source build_${1}/install/bin/bigdftvars.sh
 
 bigdft -h
 bigdft-tool -h
