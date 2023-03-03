5e7b1b60274e03e5d80453e35407e0463926563e
William Dawson <william.dawson@riken.jp>
2023-03-03 18:43:24 +0900

    Try both

--- install.sh
+++ install.sh
@@ -1,22 +1,43 @@
-echo "Hello World!"
 
-# Brew dependencies
+##############################################################################
+# Setup your computer
+##############################################################################
 brew reinstall gcc
 brew link --overwrite gcc
 brew install lapack
 brew install open-mpi
 brew install automake
 
-# Get from git
+##############################################################################
+# Get BigDFT from Git
+##############################################################################
 git clone https://gitlab.com/l_sim/bigdft-suite.git
 
+##############################################################################
+# Option 1: Build with GCC
+##############################################################################
+
 # Build directory
-mkdir build
-cd build
+mkdir build_gcc
+cd build_gcc
 
 # Autogen + build
 python3 ../bigdft-suite/Installer.py -y autogen 
 python3 ../bigdft-suite/Installer.py -y build \
-         -f ../bigdft-suite/rcfiles/macos_gcc.rc 
+         -f ../macos_gcc.rc 
+cd ../
 
-cat config.log
+##############################################################################
+# Option 2: Build with Clang
+##############################################################################
+
+# Build directory
+mkdir build_clang
+cd build_clang
+
+# Autogen + build
+python3 ../bigdft-suite/Installer.py -y autogen 
+python3 ../bigdft-suite/Installer.py -y build \
+         -f ../macos_clang.rc 
+cd ../
+
--- test.sh
+++ test.sh
@@ -1,12 +1,22 @@
 set -ex
 
-# Setup the environment
-source bigdftvars.sh
+##############################################################################
+# Option 1: GCC
+##############################################################################
+source build_gcc/install/bin/bigdftvars.sh
 
-# Check a few of the useful tools
 bigdft -h
 bigdft-tool -h
 bader -h
-
-# Check that PyBigDFT is Working
 python test.py
+
+##############################################################################
+# Option 2: Clang
+##############################################################################
+source build_clang/install/bin/bigdftvars.sh
+
+bigdft -h
+bigdft-tool -h
+bader -h
+python test.py
+
