3dc9cfc3ed48382658befba1b146d26d5fb80a8c
William Dawson <william.dawson@riken.jp>
2023-03-03 14:25:24 +0900

    http clone

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 echo "Hello World!"
-git clone git@gitlab.com:l_sim/bigdft-suite.git
+git clone https://gitlab.com/l_sim/bigdft-suite.git
 mkdir build
 python ../bigdft-suite/Installer.py -y autogen 
